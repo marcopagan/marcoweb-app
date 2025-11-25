45e6b624150c8894f342db45ee343b8f17c4ae80
marcopagan <marco.pagan.one@gmail.com>
2025-11-25 17:46:24 +0100

    Fixing grid theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,209 +1,226 @@
 <script lang="ts">
-	// IMPORT
-	const { data } = $props();
-	const { STARTING_RECTOS } = data;
-	import type { RectoType } from '$lib/types/RectoType';
-	import { STEP, snap, getCoordinates, convertCoordinates } from '$lib/rectolib';
-	import Recto from '$lib/components/Recto.svelte';
-	//import Drawer from '$lib/components/Drawer.svelte';
-	import OptionsBox from '$lib/components/OptionsBox.svelte';
-	import { options } from '$lib/shared.svelte';
-	import type { ColorScheme } from '$lib/types/ColorScheme.js';
-	import useLocalStorage from '$lib/store.svelte.js';
-	import { browser } from '$app/environment';
-	import type { AppOptions } from '$lib/types/AppOptions.js';
-	import { onMount } from 'svelte';
+  // IMPORT
+  const { data } = $props();
+  const { STARTING_RECTOS } = data;
+  import type { RectoType } from "$lib/types/RectoType";
+  import {
+    STEP,
+    snap,
+    getCoordinates,
+    convertCoordinates,
+  } from "$lib/rectolib";
+  import Recto from "$lib/components/Recto.svelte";
+  //import Drawer from '$lib/components/Drawer.svelte';
+  import OptionsBox from "$lib/components/OptionsBox.svelte";
+  import { options } from "$lib/shared.svelte";
+  import type { ColorScheme } from "$lib/types/ColorScheme.js";
+  import useLocalStorage from "$lib/store.svelte.js";
+  import { browser } from "$app/environment";
+  import type { AppOptions } from "$lib/types/AppOptions.js";
+  import { onMount } from "svelte";
 
-	// CONSTANTS
-	const localOptions = useLocalStorage('options', options);
-	// VARIABLES
-	let optionsLoaded = $state(false);
-	// Windows properties
-	let innerWidth = $state(0);
-	let innerHeight = $state(0);
-	let isDraggable = $state(false);
-	// Coordinates
-	let m1 = $state({ x: 0, y: 0 });
-	let m2 = $state({ x: 0, y: 0 });
-	let tempRecto = $state({ x: 0, y: 0, wdt: 0, hgt: 0 });
-	// Recto params
-	let liveIsValid = $state(false);
-	let counterId = $state(0);
-	let liverecto: RectoType = $state({
-		id: 9999,
-		x: -100,
-		y: -100,
-		wdt: 0,
-		hgt: 0,
-		visibility: true
-	});
-	let permarectos: RectoType[] = $state([]);
+  // CONSTANTS
+  const localOptions = useLocalStorage("options", options);
+  // VARIABLES
+  let optionsLoaded = $state(false);
+  // Windows properties
+  let innerWidth = $state(0);
+  let innerHeight = $state(0);
+  let isDraggable = $state(false);
+  // Coordinates
+  let m1 = $state({ x: 0, y: 0 });
+  let m2 = $state({ x: 0, y: 0 });
+  let tempRecto = $state({ x: 0, y: 0, wdt: 0, hgt: 0 });
+  // Recto params
+  let liveIsValid = $state(false);
+  let counterId = $state(0);
+  let liverecto: RectoType = $state({
+    id: 9999,
+    x: -100,
+    y: -100,
+    wdt: 0,
+    hgt: 0,
+    visibility: true,
+  });
+  let permarectos: RectoType[] = $state([]);
 
-	// FUNCTIONS
-	function handleDown(event: MouseEvent | TouchEvent) {
-		isDraggable = true;
-		const { x, y } = getCoordinates(event);
-		m1 = { x: x, y: y };
-		tempRecto = convertCoordinates(m1.x, m1.y, m1.x, m1.y, options.snapToGrid);
-		liverecto = { ...liverecto, x: tempRecto.x, y: tempRecto.y };
-		//if (event instanceof TouchEvent) event.preventDefault();
-	}
+  // FUNCTIONS
+  function handleDown(event: MouseEvent | TouchEvent) {
+    isDraggable = true;
+    const { x, y } = getCoordinates(event);
+    m1 = { x: x, y: y };
+    tempRecto = convertCoordinates(m1.x, m1.y, m1.x, m1.y, options.snapToGrid);
+    liverecto = { ...liverecto, x: tempRecto.x, y: tempRecto.y };
+    //if (event instanceof TouchEvent) event.preventDefault();
+  }
 
-	function handleMove(event: MouseEvent | TouchEvent) {
-		if (isDraggable) {
-			const { x, y } = getCoordinates(event);
-			m2 = { x: x, y: y };
-			tempRecto = convertCoordinates(m1.x, m1.y, m2.x, m2.y, options.snapToGrid);
-			liveIsValid = tempRecto.wdt >= STEP && tempRecto.hgt >= STEP * 2;
-			liverecto = {
-				...liverecto,
-				x: tempRecto.x,
-				y: tempRecto.y,
-				wdt: tempRecto.wdt,
-				hgt: tempRecto.hgt
-			};
-			//if (event instanceof TouchEvent) event.preventDefault();
-		}
-	}
+  function handleMove(event: MouseEvent | TouchEvent) {
+    if (isDraggable) {
+      const { x, y } = getCoordinates(event);
+      m2 = { x: x, y: y };
+      tempRecto = convertCoordinates(
+        m1.x,
+        m1.y,
+        m2.x,
+        m2.y,
+        options.snapToGrid
+      );
+      liveIsValid = tempRecto.wdt >= STEP && tempRecto.hgt >= STEP * 2;
+      liverecto = {
+        ...liverecto,
+        x: tempRecto.x,
+        y: tempRecto.y,
+        wdt: tempRecto.wdt,
+        hgt: tempRecto.hgt,
+      };
+      //if (event instanceof TouchEvent) event.preventDefault();
+    }
+  }
 
-	function handleUp(event: MouseEvent | TouchEvent) {
-		if (isDraggable) {
-			isDraggable = false;
-			liverecto = { ...liverecto, x: -100, y: -100, wdt: 0, hgt: 0 };
-			if (liveIsValid && counterId < STARTING_RECTOS.length) {
-				let tRecto = {
-					...STARTING_RECTOS[counterId],
-					x: tempRecto.x,
-					y: tempRecto.y,
-					wdt: tempRecto.wdt,
-					hgt: tempRecto.hgt
-				};
-				permarectos.push(tRecto);
-				counterId += 1;
-			} else if (liveIsValid) {
-				// OVERLAP RECTOS 
-				/*let index = counterId % STARTING_RECTOS.length;
+  function handleUp(event: MouseEvent | TouchEvent) {
+    if (isDraggable) {
+      isDraggable = false;
+      liverecto = { ...liverecto, x: -100, y: -100, wdt: 0, hgt: 0 };
+      if (liveIsValid && counterId < STARTING_RECTOS.length) {
+        let tRecto = {
+          ...STARTING_RECTOS[counterId],
+          x: tempRecto.x,
+          y: tempRecto.y,
+          wdt: tempRecto.wdt,
+          hgt: tempRecto.hgt,
+        };
+        permarectos.push(tRecto);
+        counterId += 1;
+      } else if (liveIsValid) {
+        // OVERLAP RECTOS
+        /*let index = counterId % STARTING_RECTOS.length;
                 let tRecto = {...STARTING_RECTOS[index], id: permarectos.length + counterId, x: tempRecto.x, y: tempRecto.y, wdt: tempRecto.wdt, hgt: tempRecto.hgt}
                 permarectos.push(tRecto);
                 counterId += 1;*/
 
-				// REPLACE RECTOS 
-				let index = counterId % STARTING_RECTOS.length;
-				permarectos[counterId - STARTING_RECTOS.length].visibility = false;
-				let tRecto = {
-					...STARTING_RECTOS[index],
-					x: tempRecto.x,
-					y: tempRecto.y,
-					wdt: tempRecto.wdt,
-					hgt: tempRecto.hgt
-				};
-				permarectos.push(tRecto);
-				counterId += 1;
-			}
-			m1 = { x: 0, y: 0 };
-			m2 = { x: 0, y: 0 };
-			liveIsValid = false;
-		}
-	}
-	setTimeout(() => {
-		for (let i = 0; i < 4; i++) {
-			setTimeout(() => {
-				permarectos.push(STARTING_RECTOS[counterId]);
-				counterId += 1;
-			}, 100 * i);
-		}
-	}, 500);
+        // REPLACE RECTOS
+        let index = counterId % STARTING_RECTOS.length;
+        permarectos[counterId - STARTING_RECTOS.length].visibility = false;
+        let tRecto = {
+          ...STARTING_RECTOS[index],
+          x: tempRecto.x,
+          y: tempRecto.y,
+          wdt: tempRecto.wdt,
+          hgt: tempRecto.hgt,
+        };
+        permarectos.push(tRecto);
+        counterId += 1;
+      }
+      m1 = { x: 0, y: 0 };
+      m2 = { x: 0, y: 0 };
+      liveIsValid = false;
+    }
+  }
+  setTimeout(() => {
+    for (let i = 0; i < 4; i++) {
+      setTimeout(() => {
+        permarectos.push(STARTING_RECTOS[counterId]);
+        counterId += 1;
+      }, 100 * i);
+    }
+  }, 500);
 
-	/*for (let i=0; i < 100; i++) {
+  /*for (let i=0; i < 100; i++) {
         setTimeout(() => {
             permarectos.push({id: counterId, x:snap(options.snapToGrid, Math.random()*(innerWidth-(STEP*4))), y:snap(options.snapToGrid, Math.random()*(innerHeight-(STEP*4))), wdt:snap(options.snapToGrid, Math.random()*400), hgt:snap(options.snapToGrid, Math.random()*200), content:``, visibility:true});
             counterId += 1;
         }, 30*i)
     }*/
-   function updateTheme(colorScheme: ColorScheme){
-		for (const [key, value] of Object.entries(colorScheme)) {
-			if (key == 'isDark'){
-				if (value){document.documentElement.style.setProperty('--gridTheme', `url(gridDark.svg)`) }
-				else{document.documentElement.style.setProperty('--gridTheme', `url(gridLight.svg)`) }
-			}
-			else{ document.documentElement.style.setProperty(`--${key}`, String(value)) }
-    	}
-   }
-	// ONMOUNT
-	onMount(() => {if (browser) {
-    	const storedOptionsString = localStorage.getItem('options');
-    	if (storedOptionsString) {
-			const storedOptions:AppOptions = JSON.parse(storedOptionsString);
-			options.colorScheme = storedOptions.colorScheme;
-			options.showBorder = storedOptions.showBorder;
-			options.showGrid = storedOptions.showGrid;
-			options.snapToGrid = storedOptions.showGrid;
-		};
-		optionsLoaded = true;
-  	}});
-	// EFFECTS
-	$effect(() => updateTheme(options.colorScheme));
-	$effect(() => localOptions.set(options));
-
+  function updateTheme(colorScheme: ColorScheme) {
+    for (const [key, value] of Object.entries(colorScheme)) {
+      document.documentElement.style.setProperty(`--${key}`, String(value));
+    }
+  }
+  // ONMOUNT
+  onMount(() => {
+    if (browser) {
+      const storedOptionsString = localStorage.getItem("options");
+      if (storedOptionsString) {
+        const storedOptions: AppOptions = JSON.parse(storedOptionsString);
+        options.colorScheme = storedOptions.colorScheme;
+        options.showBorder = storedOptions.showBorder;
+        options.showGrid = storedOptions.showGrid;
+        options.snapToGrid = storedOptions.showGrid;
+      }
+      optionsLoaded = true;
+    }
+  });
+  // EFFECTS
+  $effect(() => updateTheme(options.colorScheme));
+  $effect(() => localOptions.set(options));
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 {#if optionsLoaded}
-<div id="background" class={options.showGrid ? 'bgrid' : ''}></div>
+  <div
+    id="background"
+    class={options.showGrid && options.colorScheme.isDark
+      ? "bgrid-dark"
+      : options.showGrid && !options.colorScheme.isDark
+        ? "bgrid-light"
+        : ""}
+  ></div>
 
-<div
-	id="foreground"
-	role="button"
-	tabindex="0"
-	onmousedown={handleDown}
-	onmousemove={handleMove}
-	onmouseup={handleUp}
-	ontouchstart={handleDown}
-	ontouchmove={handleMove}
-	ontouchend={handleUp}
->
-	<Recto
-		id="r-{liverecto.id}"
-		type={liveIsValid ? 'live' : 'invalid'}
-		x={liverecto.x}
-		y={liverecto.y}
-		wdt={liverecto.wdt}
-		hgt={liverecto.hgt}><p style="display: none"></p></Recto
-	>
+  <div
+    id="foreground"
+    role="button"
+    tabindex="0"
+    onmousedown={handleDown}
+    onmousemove={handleMove}
+    onmouseup={handleUp}
+    ontouchstart={handleDown}
+    ontouchmove={handleMove}
+    ontouchend={handleUp}
+  >
+    <Recto
+      id="r-{liverecto.id}"
+      type={liveIsValid ? "live" : "invalid"}
+      x={liverecto.x}
+      y={liverecto.y}
+      wdt={liverecto.wdt}
+      hgt={liverecto.hgt}><p style="display: none"></p></Recto
+    >
 
-	{#each permarectos as recto}
-		{#if recto.content === 'BUTTON' && recto.visibility}
-			<Recto
-				id="r-{recto.id}"
-				type={options.showBorder ? 'perma' : 'transparent'}
-				x={recto.x}
-				y={recto.y}
-				wdt={recto.wdt}
-				hgt={recto.hgt}
-			>
-				<h3>GO TO</h3>
-				<p>
-					<button class="disabled-btn">Selected projects<br><span>(coming soon)</span></button>
-					<!--<button onclick={() => (options.openDrawer = !options.openDrawer)}>Selected projects (soon)</button>
+    {#each permarectos as recto}
+      {#if recto.content === "BUTTON" && recto.visibility}
+        <Recto
+          id="r-{recto.id}"
+          type={options.showBorder ? "perma" : "transparent"}
+          x={recto.x}
+          y={recto.y}
+          wdt={recto.wdt}
+          hgt={recto.hgt}
+        >
+          <h3>GO TO</h3>
+          <p>
+            <button class="disabled-btn"
+              >Selected projects<br /><span>(coming soon)</span></button
+            >
+            <!--<button onclick={() => (options.openDrawer = !options.openDrawer)}>Selected projects (soon)</button>
 					<br /><button onclick={() => (options.openDrawer = !options.openDrawer)}>CV</button>-->
-				</p>
-			</Recto>
-		{:else if recto.visibility}
-			<Recto
-				id="r-{recto.id}"
-				type={options.showBorder ? 'perma' : 'transparent'}
-				x={recto.x}
-				y={recto.y}
-				wdt={recto.wdt}
-				hgt={recto.hgt}
-			>
-				{@html recto.content}
-			</Recto>
-		{/if}
-	{/each}
-</div>
+          </p>
+        </Recto>
+      {:else if recto.visibility}
+        <Recto
+          id="r-{recto.id}"
+          type={options.showBorder ? "perma" : "transparent"}
+          x={recto.x}
+          y={recto.y}
+          wdt={recto.wdt}
+          hgt={recto.hgt}
+        >
+          {@html recto.content}
+        </Recto>
+      {/if}
+    {/each}
+  </div>
 
-<!--<div id="drawer-wrapper" class={options.openDrawer ? 'drawer-expanded' : ''}>
+  <!--<div id="drawer-wrapper" class={options.openDrawer ? 'drawer-expanded' : ''}>
 	<div id="drawer-backdrop"></div>
 	<button
 		id="drawer-button"
@@ -212,14 +229,21 @@
 	></button>
 	<Drawer projects={projects}/>
 </div>-->
-<OptionsBox />
+  <OptionsBox />
 {/if}
-<style>
-	::selection {color: unset}
-	::-moz-selection {color: unset}
-	div:focus {outline: none}
 
-	/*#drawer-wrapper {
+<style>
+  ::selection {
+    color: unset;
+  }
+  ::-moz-selection {
+    color: unset;
+  }
+  div:focus {
+    outline: none;
+  }
+
+  /*#drawer-wrapper {
 		position: absolute;
 		left: 0;
 		bottom: -100vh;
@@ -239,7 +263,7 @@
 		right: 0;
 		bottom: 0;
 		/*background: linear-gradient(0deg, var(--liveborder) 0%, var(--permaborder) 100%);*/
-		/*mix-blend-mode: difference;
+  /*mix-blend-mode: difference;
 		backdrop-filter: blur(5px);
 		z-index: 0;
 	}
@@ -257,10 +281,17 @@
 		bottom: 0vh !important;
 	}*/
 
-	.disabled-btn{
-		text-align: left;
-		line-height: 1.15;
-		span{font-family: 'APL386', monospace; text-transform: uppercase; font-size: 1rem; letter-spacing: -.05rem;}
-		&:hover{text-decoration: line-through;}
-	}
+  .disabled-btn {
+    text-align: left;
+    line-height: 1.15;
+    span {
+      font-family: "APL386", monospace;
+      text-transform: uppercase;
+      font-size: 1rem;
+      letter-spacing: -0.05rem;
+    }
+    &:hover {
+      text-decoration: line-through;
+    }
+  }
 </style>
