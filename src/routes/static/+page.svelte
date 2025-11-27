250342701002989edba2ac19982203061dca0815
marcopagan <marco.pagan.one@gmail.com>
2025-11-27 19:52:34 +0100

    Adding static

---
+++ src/routes/static/+page.svelte
@@ -0,0 +1,81 @@
+<div class="recto">
+  <div class="recto-inside">
+    <p>Marco Pagan<br /><i>Information design / coding</i></p>
+  </div>
+</div>
+
+<div class="recto">
+  <div class="recto-inside">
+    <p style="max-width: 26rem">
+      I'm interested in learning, displaying information and parametric design.
+      My practice intersect coding and graphic design to produce both printed
+      and digital content.
+    </p>
+  </div>
+</div>
+
+<div class="recto">
+  <div class="recto-inside">
+    <h3>CONTACTS</h3>
+    <p><a href="mailto:marco.pagan.one@gmail.com">Mail</a></p>
+    <p>
+      <a href="http://www.instagram.com/marcopagan" target="_blank">Instagram</a
+      >
+    </p>
+  </div>
+</div>
+
+<div class="recto">
+  <div class="recto-inside">
+    <p>
+      Currently at <a href="https://calib.ro" target="_blank">Studio Calibro</a>
+    </p>
+  </div>
+</div>
+
+<div class="recto">
+  <div class="recto-inside">
+    <p>
+      BA Graphic Design at <a href="https://isiaurbino.net" target="_blank"
+        >ISIA U</a
+      >
+      (IT).<br /> MA Information Design at
+      <a href="https://www.designacademy.nl/" target="_blank">DAE</a>
+      (NL).<br /> You can explore ma MA thesis from here:
+      <a href="http://avoidingthebrick.marcopagan.com" target="_blank"
+        >Avoiding the Brick</a
+      >
+    </p>
+  </div>
+</div>
+
+<div class="recto">
+  <div class="recto-inside">
+    <p>Open for collaboration for:</p>
+    <ul>
+      <li>Information Design</li>
+      <li>Frontend Design/Development</li>
+      <li>Creative Coding</li>
+      <li>Graphic Design</li>
+    </ul>
+  </div>
+</div>
+
+<style>
+  .recto {
+    font-family: "TimesNewerRoman", "Times New Roman", Times, serif;
+    font-size: var(--m);
+    text-rendering: optimizeSpeed;
+    background-color: var(--perma);
+    border-radius: var(--border-rad);
+    line-height: 1.2;
+    letter-spacing: -0.01rem;
+    width: fit-content;
+    margin-top: 1rem;
+    margin-left: 1rem;
+    max-width: calc(100vw - 2rem);
+  }
+  .recto-inside {
+    width: fit-content;
+  }
+</style>
